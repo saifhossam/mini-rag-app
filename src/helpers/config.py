3081a42ea8a65ecfff8771c8a9ddb5dc42fc3932
saifhossam <saifhossamaldin@gmail.com>
2026-06-18 17:08:22 +0300

    .

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -27,6 +27,10 @@ class Settings(BaseSettings):
     INPUT_DEFAULT_MAX_CHARACTERS: int
     GENERATION_DEFAULT_MAX_TOKENS: int
     GENERATION_DEFAULT_TEMPERATURE: float
+
+    VECTOR_DB_BACKEND : str
+    VECTOR_DB_PATH : str
+    VECTOR_DB_DISTANCE_METHOD: str = None
     
     class Config:
         env_file = ".env"
